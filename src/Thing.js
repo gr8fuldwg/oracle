e7af91d073a7ca7b91a771d05243446fcf5c6898
gr8fuldwg <gr8fuldwg@gmail.com>
2019-06-29 20:08:45 -0400

    added constructor to 'Thing component' to manage state as to reveal answer using onClick function for Oracle Button

--- src/Thing.js
+++ src/Thing.js
@@ -1,33 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Answer from "./Answers";
 
-function Thing() {
-  return (
-    <div className="Thing">
-      <div className="input-group mb-3">
-        <div className="input-group-prepend">
-          <button
-            className="btn btn-outline-secondary"
-            type="button"
-            id="button-addon1"
-          >
-            Ask The Oracle
-          </button>
+class Thing extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: false };
+
+    this.answer = this.answer.bind(this);
+  }
+
+  answer = () => {
+    const { show } = this.state;
+    this.setState({ show: !show });
+  };
+
+  render() {
+    return (
+      <div className="Thing">
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            <button
+              onClick={this.answer}
+              className="btn btn-outline-secondary"
+              type="button"
+              id="button-addon1"
+            >
+              Ask The Oracle
+            </button>
+            
+          </div>
+          <input
+            type="text"
+            className="form-control"
+            placeholder=""
+            aria-label="Example text with button addon"
+            aria-describedby="button-addon1"
+          />
         </div>
-        <input
-          type="text"
-          className="form-control"
-          placeholder=""
-          aria-label="Example text with button addon"
-          aria-describedby="button-addon1"
-        />
+        {this.state.show && <Box />}
       </div>
+    );
+  }
+}
+
+class Box extends Component {
+  render() {
+    return (
       <blockquote>
         <Answer className="text-center" />
       </blockquote>
-    </div>
-  );
+    );
+  }
 }
 
 export default Thing;
